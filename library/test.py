90411727c82962299c0cfce3bca327f80c7a6aac
Phil Howard <phil@gadgetoid.com>
2019-04-12 11:42:52 +0100

    More complete test

--- library/test.py
+++ library/test.py
@@ -1,4 +1,18 @@
+import time
 from ads1015 import ADS1015
+
+
 ads1015 = ADS1015()
-value = ads1015._ads1015.CONV.get_value()
-print(value)
+ads1015.set_multiplexer('in0/ref')
+ads1015.set_mode('single')
+ads1015.set_programmable_gain(2.048)
+
+while True:
+    ads1015.set_status('active')
+    while ads1015.get_status():
+        time.sleep(0.001)
+
+    # value = ads1015._ads1015.CONV.get_value()
+    value = ads1015.get_conversion_value()
+    print(value)
+    time.sleep(0.5)
