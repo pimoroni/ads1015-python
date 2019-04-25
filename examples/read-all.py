47599c4265392f716558a30bab59b88f22dcbf51
Phil Howard <phil@gadgetoid.com>
2019-04-25 15:17:14 +0100

    Basic read all example

---
+++ examples/read-all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import time
+from ads1015 import ADS1015
+
+CHANNELS = ['in0/ref', 'in1/ref', 'in2/ref']
+
+print("""read-all.py - read all three inputs of the ADC
+
+Press Ctrl+C to exit!
+""")
+
+ads1015 = ADS1015()
+ads1015.set_mode('single')
+ads1015.set_programmable_gain(2.048)
+ads1015.set_sample_rate(1600)
+
+reference = ads1015.get_reference_voltage()
+
+print("Reference voltage: {:6.3f}v \n".format(reference))
+
+try:
+    while True:
+        for channel in CHANNELS:
+            value = ads1015.get_compensated_voltage(channel=channel, reference_voltage=reference)
+            print("{}: {:6.3f}v".format(channel, value))
+
+        print("")
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    pass
